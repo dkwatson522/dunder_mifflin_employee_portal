56b40edbe8d34c4ce17d3f5cd7ab4be4303684c2
Daniel Watson <dkwatson522gmail.com>
2021-04-08 23:37:30 -0400

    set up user dashboard

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,17 @@
 class UserController < ApplicationController
+  before_action :load_user, only: [:index, :show, :edit, :update, :destroy]
+
   def index
-    @user = User.all
+    users = User.all
+    @user = users.sample
   end
+
+  def edit
+  end
+
+  def show
+  end
+
   def new
     @user = User.new
   end
@@ -14,5 +24,12 @@ class UserController < ApplicationController
     else
       render :index
     end
+  end
+
+  private
+  def load_user
+      user_id = params[:id]
+
+      @user = User.find_by(id: user_id)
   end
 end
