80179c1d3a12217a8de9e050deb94589d23e3ac9
Daniel Watson <dkwatson522gmail.com>
2021-04-29 18:05:08 -0400

    cloudinary production config

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -5,3 +5,11 @@ Cloudinary.config do |config|
   config.secure = true
   config.cdn_subdomain = true
 end
+
+Cloudinary.config do |config|
+  config.cloud_name = Rails.application.credentials[:cloudinary][:production][:cloud_name]
+  config.api_key = Rails.application.credentials[:cloudinary][:production][:api_key]
+  config.api_secret = Rails.application.credentials[:cloudinary][:production][:api_secret]
+  config.secure = true
+  config.cdn_subdomain = true
+end
