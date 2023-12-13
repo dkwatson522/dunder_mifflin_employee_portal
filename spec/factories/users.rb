9796fba54a31a60533b79371ee21e8816be3bb8a
Daniel Watson <dkwatson522gmail.com>
2023-12-13 08:13:49 -0500

    Add annotate gem

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -30,27 +30,15 @@
 #
 #  fk_rails_...  (manager_id => users.id)
 #
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :validatable
-
-
-  has_many :employees, dependent: :destroy, class_name: 'User', foreign_key: 'manager_id', inverse_of: :manager
-  has_one_attached :avatar
-  belongs_to :manager, class_name: 'User', optional: true, inverse_of: :employees
-  has_many :requests, dependent: :destroy
-
-  # def serializable_hash(options=nil)
-  #   super({
-  #     only: [],
-  #     methods: [:user_avatar_url]
-  #   }.merge(options || {}))
-  # end
-  #
-  # def user_avatar_url
-  #   Rails.application.routes.url_helpers.url_for( user.avatar )
-  # end
-  # ...
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    role { Faker::Job.title }
+    department { Faker::Job.field }
+    experience { Faker::Number.between(from: 0, to: 10) }
+    is_admin { true }
+    email { Faker::Internet.email }
+    image_url {}
+    password { '123456' }
+  end
 end
--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: requests
+#
+#  id              :bigint           not null, primary key
+#  end_date        :string
+#  is_approved     :boolean          default(FALSE)
+#  number_of_hours :float
+#  start_date      :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  user_id         :bigint           not null
+#
+# Indexes
+#
+#  index_requests_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require "test_helper"
 
 class RequestTest < ActiveSupport::TestCase
